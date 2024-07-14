26ff4a74e491d7b54d1b779686a475666be72e9e
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2024-07-14 21:29:16 +0300

    Add result div and logic to display it

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-/*
- ** The Gameboard represents the state of the board
- ** Each square holds a Cell (defined later)
- ** and we expose a dropToken method to be able to add Cells to squares
- */
-
 function Gameboard() {
   const rows = 3;
   const columns = 3;
   const board = [];
 
-  // Create a 2d array that will represent the state of the game board
-  // For this 2d array, row 0 will represent the top row and
-  // column 0 will represent the left-most column.
-  // This nested-loop technique is a simple and common way to create a 2d array.
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
@@ -20,20 +10,14 @@ function Gameboard() {
     }
   }
 
-  // This will be the method of getting the entire board that our
-  // UI will eventually need to render it.
   const getBoard = () => board;
 
-  // In order to place a token, we need to find an empty cell, *then* change that cell's value to the player token
-  const placeToken = (row, column, player) => {
+  const placeMarker = (row, column, playerMarker) => {
     if (board[row][column].getValue() === "") {
-      board[row][column].addToken(player);
+      board[row][column].addMarker(playerMarker);
     }
   };
 
-  // This method will be used to print our board to the console.
-  // It is helpful to see what the board looks like after each turn as we play,
-  // but we won't need it after we build our UI
   const printBoard = () => {
     const boardWithCellValues = board.map((row) =>
       row.map((cell) => cell.getValue())
@@ -41,54 +25,36 @@ function Gameboard() {
     console.log(boardWithCellValues);
   };
 
-  // Here, we provide an interface for the rest of our
-  // application to interact with the board
-  return { getBoard, placeToken, printBoard };
+  return { getBoard, placeMarker, printBoard };
 }
-
-/*
- ** A Cell represents one "square" on the board and can have one of
- ** "": no token is in the square,
- ** "X": Player One's token,
- ** "O": Player Two's token
- */
 
 function Cell() {
   let value = "";
 
-  // Accept a player's token to change the value of the cell
-  const addToken = (player) => {
-    value = player;
-  };
+  const addMarker = (playerMarker) => (value = playerMarker);
 
-  // How we will retrieve the current value of this cell through closure
   const getValue = () => value;
 
   return {
-    addToken,
+    addMarker,
     getValue,
   };
 }
 
-/*
- ** The GameController will be responsible for controlling the
- ** flow and state of the game's turns, as well as whether
- ** anybody has won the game
- */
 function GameController(
-  playerOneName = "Player One",
-  playerTwoName = "Player Two"
+  playerOneName = "Player X",
+  playerTwoName = "Player O"
 ) {
   const gameboard = Gameboard();
 
   const players = [
     {
       name: playerOneName,
-      token: "X",
+      marker: "X",
     },
     {
       name: playerTwoName,
-      token: "O",
+      marker: "O",
     },
   ];
 
@@ -104,30 +70,30 @@ function GameController(
     console.log(`${getActivePlayer().name}'s turn.`);
   };
 
+  const getBoard = () => gameboard.getBoard();
+
   const checkForWin = (row, column, player) => {
     const gameBoard = gameboard.getBoard();
-    // Row check
+
     if (gameBoard[row].every((cell) => cell.getValue() === player)) {
       return true;
     }
 
-    // Column check
     if (gameBoard.every((row) => row[column].getValue() === player)) {
       return true;
     }
 
-    // Primary diagonal check (top-left to bottom-right)
     if (row === column) {
       if (gameBoard.every((row, column) => row[column].getValue() === player)) {
         return true;
       }
     }
 
-    // Secondary diagonal check (top-right to bottom-left)
     if (row + column === gameBoard.length - 1) {
       if (
         gameBoard.every(
-          (row, index) => row[gameBoard.length - 1 - index].getValue() === player
+          (row, index) =>
+            row[gameBoard.length - 1 - index].getValue() === player
         )
       ) {
         return true;
@@ -138,46 +104,120 @@ function GameController(
   };
 
   const checkForDraw = () => {
-    return gameboard.getBoard().every(row => 
-      row.every(cell => cell.getValue() !== "")
-    );
+    return gameboard
+      .getBoard()
+      .every((row) => row.every((cell) => cell.getValue() !== ""));
   };
 
+  let gameActive = true;
+
+  const getGameState = () => gameActive;
+
   const playRound = (row, column) => {
-    // Place a token for the current player
+    if (!gameActive) return;
+
     console.log(
-      `Placing ${getActivePlayer().name}'s token into cell (${row}, ${column})...`
+      `Placing ${
+        getActivePlayer().name
+      }'s marker into cell (${row}, ${column})...`
     );
-    gameboard.placeToken(row, column, getActivePlayer().token);
-    /*  This is where we would check for a winner and handle that logic,
-        such as a win message. */
-    if (checkForWin(row, column, getActivePlayer().token)) {
-      // Handle win (e.g., stop the game, display a message, etc.)
+    gameboard.placeMarker(row, column, getActivePlayer().marker);
+
+    if (checkForWin(row, column, getActivePlayer().marker)) {
       gameboard.printBoard();
       console.log(`${getActivePlayer().name} wins!`);
+      gameActive = false;
       return;
     }
 
     if (checkForDraw()) {
       gameboard.printBoard();
       console.log("It's a draw!");
+      gameActive = false;
       return;
     }
 
-    // Switch player turn
     switchPlayerTurn();
     printNewRound();
   };
 
-  // Initial play game message
   printNewRound();
 
-  // For the console version, we will only use playRound, but we will need
-  // getActivePlayer for the UI version, so I'm revealing it now
   return {
+    getBoard,
     playRound,
     getActivePlayer,
+    getGameState,
+    checkForWin,
+    checkForDraw,
   };
 }
 
-const game = GameController();
+(function displayController() {
+  const game = GameController();
+  const playerTurnDiv = document.querySelector('.turn');
+  const boardDiv = document.querySelector('.board');
+  const resultDiv = document.querySelector('.result');
+
+  const updateScreen = () => {
+    // clear the board
+    boardDiv.textContent = "";
+
+    // get the newest version of the board and player turn
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    // Display player's turn
+    if (game.getGameState()) {
+      playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+    } else {
+      playerTurnDiv.style.display = "none";
+    }
+
+    // Render board squares
+    board.forEach((row) => {
+      row.forEach((cell, index) => {
+        // Anything clickable should be a button!!
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        // Create a data attribute to identify the column
+        // This makes it easier to pass into our `playRound` function
+        cellButton.dataset.row = board.indexOf(row);
+        cellButton.dataset.column = index;
+        cellButton.textContent = cell.getValue();
+        
+        boardDiv.appendChild(cellButton);
+      });
+    });
+  };
+
+  // Add event listener for the board
+  boardDiv.addEventListener("click", (e) => {
+    const selectedRow = e.target.dataset.row;
+    const selectedColumn = e.target.dataset.column;
+    // Make sure I've clicked a column and not the gaps in between
+    if (!selectedColumn || !selectedRow) return;
+
+    // Prevent clicking on a cell that already has a marker
+    const board = game.getBoard();
+    if (board[selectedRow][selectedColumn].getValue() !== "") return;
+
+    game.playRound(selectedRow, selectedColumn);
+    updateScreen();
+
+     // Display result message if game has ended
+     if (!game.getGameState()) {
+      const activePlayer = game.getActivePlayer();
+      if (game.checkForWin(selectedRow, selectedColumn, activePlayer.marker)) {
+        resultDiv.textContent = `${activePlayer.name} wins!`;
+      } else if (game.checkForDraw()) {
+        resultDiv.textContent = "It's a draw!";
+      }
+    }
+  });
+
+  // Initial render
+  updateScreen();
+
+  // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+})();
