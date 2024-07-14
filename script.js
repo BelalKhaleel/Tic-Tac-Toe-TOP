5e92d6103260f77c5394ad3cdbb9dc204765e951
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2024-07-14 21:31:16 +0300

    Remove unnecessary comments

--- script.js
+++ script.js
@@ -155,58 +155,47 @@ function GameController(
 
 (function displayController() {
   const game = GameController();
-  const playerTurnDiv = document.querySelector('.turn');
-  const boardDiv = document.querySelector('.board');
-  const resultDiv = document.querySelector('.result');
+  const playerTurnDiv = document.querySelector(".turn");
+  const boardDiv = document.querySelector(".board");
+  const resultDiv = document.querySelector(".result");
 
   const updateScreen = () => {
-    // clear the board
     boardDiv.textContent = "";
 
-    // get the newest version of the board and player turn
     const board = game.getBoard();
     const activePlayer = game.getActivePlayer();
 
-    // Display player's turn
     if (game.getGameState()) {
       playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
     } else {
       playerTurnDiv.style.display = "none";
     }
 
-    // Render board squares
     board.forEach((row) => {
       row.forEach((cell, index) => {
-        // Anything clickable should be a button!!
         const cellButton = document.createElement("button");
         cellButton.classList.add("cell");
-        // Create a data attribute to identify the column
-        // This makes it easier to pass into our `playRound` function
         cellButton.dataset.row = board.indexOf(row);
         cellButton.dataset.column = index;
         cellButton.textContent = cell.getValue();
-        
+
         boardDiv.appendChild(cellButton);
       });
     });
   };
 
-  // Add event listener for the board
   boardDiv.addEventListener("click", (e) => {
     const selectedRow = e.target.dataset.row;
     const selectedColumn = e.target.dataset.column;
-    // Make sure I've clicked a column and not the gaps in between
     if (!selectedColumn || !selectedRow) return;
 
-    // Prevent clicking on a cell that already has a marker
     const board = game.getBoard();
     if (board[selectedRow][selectedColumn].getValue() !== "") return;
 
     game.playRound(selectedRow, selectedColumn);
     updateScreen();
 
-     // Display result message if game has ended
-     if (!game.getGameState()) {
+    if (!game.getGameState()) {
       const activePlayer = game.getActivePlayer();
       if (game.checkForWin(selectedRow, selectedColumn, activePlayer.marker)) {
         resultDiv.textContent = `${activePlayer.name} wins!`;
@@ -216,8 +205,5 @@ function GameController(
     }
   });
 
-  // Initial render
   updateScreen();
-
-  // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
 })();
